5519e41951316ee0c1f0041696611c48b39eaf43
Muhammet Soyturk <msoyturk20@ku.edu.tr>
2022-10-26 20:45:03 +0300

    get all symbols

--- tools/futures.py
+++ tools/futures.py
@@ -3,7 +3,7 @@ import json
 from binance.um_futures import UMFutures
 from binance.lib.utils import config_logging
 
-config_logging(logging, logging.DEBUG)
+#config_logging(logging, logging.DEBUG)
 
 def get_open_interest(symbol="BTCUSDT"):
     futures_client = UMFutures()
@@ -25,6 +25,14 @@ def get_top_long_short_position_ratio(symbol="BTCUSDT", period="5m"):
     futures_client = UMFutures()
     return futures_client.top_long_short_position_ratio(symbol, period)
 
+def get_all_symbols():
+    futures_client = UMFutures()
+    symbols_info = futures_client.exchange_info()['symbols']
+    symbols = [symbol['symbol'] for symbol in symbols_info]
+    return symbols
+
 if __name__ == '__main__':
-    print(json.dumps(get_top_long_short_account_ratio(), indent=4))
-    print(json.dumps(get_top_long_short_position_ratio(), indent=4))
+    #print(json.dumps(get_top_long_short_account_ratio(), indent=4))
+    #print(json.dumps(get_top_long_short_position_ratio(), indent=4))
+
+    print(sorted(get_all_symbols()))
