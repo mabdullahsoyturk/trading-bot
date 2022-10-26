c38e856eb2ba36f2a8b1249e931a7705f9078f50
Muhammet Soyturk <msoyturk20@ku.edu.tr>
2022-10-26 20:38:00 +0300

    get useful information like funding rate, open interest etc.

---
+++ tools/futures.py
@@ -0,0 +1,30 @@
+import logging
+import json
+from binance.um_futures import UMFutures
+from binance.lib.utils import config_logging
+
+config_logging(logging, logging.DEBUG)
+
+def get_open_interest(symbol="BTCUSDT"):
+    futures_client = UMFutures()
+    return futures_client.open_interest("BTCUSDT")["openInterest"]
+
+def get_funding_rate(symbol="BTCUSDT"):
+    futures_client = UMFutures()
+    return futures_client.funding_rate("BTCUSDT", **{"limit": 1})
+
+def get_long_short_account_ratio(symbol="BTCUSDT", period="5m"):
+    futures_client = UMFutures()
+    return futures_client.long_short_account_ratio(symbol, period)
+
+def get_top_long_short_account_ratio(symbol="BTCUSDT", period="5m"):
+    futures_client = UMFutures()
+    return futures_client.top_long_short_account_ratio(symbol, period)
+
+def get_top_long_short_position_ratio(symbol="BTCUSDT", period="5m"):
+    futures_client = UMFutures()
+    return futures_client.top_long_short_position_ratio(symbol, period)
+
+if __name__ == '__main__':
+    print(json.dumps(get_top_long_short_account_ratio(), indent=4))
+    print(json.dumps(get_top_long_short_position_ratio(), indent=4))
